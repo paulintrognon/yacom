0a10745c06157a6edf7e9ce80a4a89e82ad83593
OGRE7 <paulin.trognon@gmail.com>
2013-09-10 20:21:49 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yacom::Application.config.secret_token = 'b58e86b1e93a12657f4821cb09e46aa5fe8b30519005bf0673e443483b299179967706159de41dc06b627e8bc1d896ff6696f27874dcf236bde06a0d03c24680'
